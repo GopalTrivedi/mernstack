36548cbcd175936025e9567a4d4929fd031b660c
Gopal Trivedi <trivedigopal666@gmail.com>
2023-01-04 23:44:32 +0530

    added code

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+let express = require('express');
+let app = express();
 
+app.get('/',(req,res) => {
+	return res.send('done')
+})
+app.listen(5000,function(){
+	console.log("Server Started on 5000")
+})
