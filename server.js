9541d7b34b150c5a311c4b10fa63056f0aa3e61b
Gopal141 <trivedigopal666@gmail.com>
2023-01-30 17:33:16 +0530

    New Commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.get('/',(req,res) => {
-	return res.send('done')
+app.get('/', (req, res) => {
+    return res.send('done by me')
 })
-app.listen(5000,function(){
-	console.log("Server Started on 5000")
+app.listen(5000, function() {
+    console.log("Server Started on 5000")
 })
