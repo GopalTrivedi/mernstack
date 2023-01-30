6f01948d83058df63738d0eaf97dd035f3c437e2
Gopal141 <trivedigopal666@gmail.com>
2023-01-30 18:07:51 +0530

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    return res.send('done by Gopal')
+    return res.send('done by Kiran')
 })
 app.listen(5000, function() {
     console.log("Server Started on 5000")
